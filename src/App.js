6c9f5d5f4ac0531c807a13ec4506b45d906e0305
rafinsripon <rafinsripon@gmail.com>
2022-09-22 22:29:21 +0600

    start gone store

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import Todos from './components/Todos/Todos';
-// import Mobile from './components/Mobile/Mobile';
-// import Countries from './components/Countries/Countries';
-
-
 
 function App() {
   return (
     <div className="App">
-      {/* <Countries></Countries> */}
-      {/* <Mobile></Mobile> */}
-      <Todos></Todos>
+      <h1>all goune</h1>
     </div>
   );
 }
